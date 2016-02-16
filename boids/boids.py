cd110a852d9d29a5e2a66fff68369cff59d98b6a
David Twomey <david.twomey@live.com>
2016-02-16 15:11:08 +0000

    Restructured boids to class design with init method

--- boids/boids.py
+++ boids/boids.py
@@ -16,45 +16,55 @@ boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 
+class Boids(object):
+	'''
+	Description: A class to model aggregate motion of a flock of simulated birds (boids)
+	'''
+	def __init__(self, positions, velocity, fields):
+		self.positions = positions
+		self.velocity = velocity
+		self.fields = fields
+		
 
+	def updateBoids(self, boids):
+		xs,ys,xvs,yvs=boids
+		# Fly towards the middle
+		for i in range(len(xs)):
+			for j in range(len(xs)):
+				xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+		for i in range(len(xs)):
+			for j in range(len(xs)):
+				yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+		# Fly away from nearby boids
+		for i in range(len(xs)):
+			for j in range(len(xs)):
+				if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+					xvs[i]=xvs[i]+(xs[i]-xs[j])
+					yvs[i]=yvs[i]+(ys[i]-ys[j])
+		# Try to match speed with nearby boids
+		for i in range(len(xs)):
+			for j in range(len(xs)):
+				if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
+					xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
+					yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+		# Move according to velocities
+		for i in range(len(xs)):
+			xs[i]=xs[i]+xvs[i]
+			ys[i]=ys[i]+yvs[i]
 
-def update_boids(boids):
-	xs,ys,xvs,yvs=boids
-	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
-	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
-
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
-
-def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
-
-
-anim = animation.FuncAnimation(figure, animate,
+	def deploySimulation(self):
+		figure=plt.figure()
+		axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
+		scatter=axes.scatter(boids[0],boids[1])
+		anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
+	def animate(self, frame):
+		update_boids(boids)
+		scatter.set_offsets(zip(boids[0],boids[1]))
+		
+
+
 
 if __name__ == "__main__":
-    plt.show()
+	obj_boid = Boids()
+	obj_boid.deploySimulation()
