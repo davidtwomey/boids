f692f9d33736f6a208625c6ce423243b659f6c7f
David Twomey <david.twomey@live.com>
2016-02-28 21:01:57 +0000

    added first unit test

--- boids/test/test_boids.py
+++ boids/test/test_boids.py
@@ -1,12 +1,21 @@
-from boids import update_boids
+import sys
+from boids.boids import Boids
 from nose.tools import assert_almost_equal
 import os
 import yaml
+import numpy as np
 
-def test_bad_boids_regression():
-    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'fixture.yml')))
-    boid_data=regression_data["before"]
-    update_boids(boid_data)
-    for after,before in zip(regression_data["after"],boid_data):
-        for after_value,before_value in zip(after,before): 
-            assert_almost_equal(after_value,before_value,delta=0.01)
+fixtures_file = os.path.join(os.path.dirname(__file__),'fixtures','fixture.yaml')
+fixtures = yaml.load(open(fixtures_file))
+
+def test_update_boids():
+	for fixture in fixtures:
+		before = fixture.pop('before')
+		after = fixture.pop('after')
+		boids = Boids()
+		boids.positions = np.array(before[0:2])
+		print boids.positions
+		boids.velocities = np.array(before[2:4])
+		boids.update_boids(boids.positions,boids.velocities)
+		assert_almost_equal(boids.positions.all(), np.array(after[0:2]).all())
+		assert_almost_equal(boids.velocities.all(), np.array(after[2:4]).all())
